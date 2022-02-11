d3fcff68a71f6f1ccd529f66be16a79c0b212c19
donlebo <99505516+donlebo@users.noreply.github.com>
2022-02-11 23:20:30 +0100

    Add files via upload

---
+++ src/app/pages/results/results.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ResultsPageRoutingModule } from './results-routing.module';
+
+import { ResultsPage } from './results.page';
+import { ExploreContainerComponentModule } from 'src/app/explore-container/explore-container.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ResultsPageRoutingModule,
+    ExploreContainerComponentModule
+  ],
+  declarations: [ResultsPage]
+})
+export class ResultsPageModule {}
